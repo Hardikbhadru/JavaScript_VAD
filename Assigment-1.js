f8379efe8db7580c81e6f0c66e54f07b86b1ae94
Hardik <hardik.bhadru1999@gmail.com>
2022-07-05 12:32:05 +0530

    Assigment-1/feature/task1_sleepIn & task2_icyhot => completed

--- Assigment-1.js
+++ Assigment-1.js
@@ -1,8 +1,26 @@
+//Task-1:- sleepIn
 function sleepIn(weekday, vacation) {
+
+    //checking the condition for validation
     if (weekday == true && vacation == false) {
         return false;
     } else {
         return true;
     }
 }
-console.log(sleepIn(true,false));
+//cosole output
+console.log(sleepIn(true, false));
+
+//Task-2:- icyHot
+function icyHot(temp1, temp2) {
+
+    //checking the condition for validation
+    if ((temp1 < 0 || temp2 < 0) && (temp1 > 100 || temp2 > 100)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//cosole output
+console.log(icyHot(2, 120));
